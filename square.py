d84b719181d9841f7caacbf0dbec420a4379ee28
Nguyen Le Cuong <nguyenlecuongaaa@gmail.com>
2023-10-10 21:01:25 +0300

    adding comment to square.py

--- square.py
+++ square.py
@@ -1,7 +1,11 @@
 
 def area(a):
+    ''' number a is the side of the square.
+Return  the area of the square of side a.'''
     return a * a
 
 
 def perimeter(a):
+   '''number a is the side of the square.
+Return to the perimeter of the square of side a.'''
     return 4 * a
