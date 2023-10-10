4462fe80c0f161f8bd7570650ba0675197dd5165
Nguyen Le Cuong <nguyenlecuongaaa@gmail.com>
2023-10-10 20:55:58 +0300

    Adding comment to triangle.py

---
+++ triangle.py
@@ -0,0 +1,10 @@
+def area(a, h):
+    ''' a is a side of a right triangle.
+h is the height lowered from the opposite peak to a.
+Return the perimeter of the right triangle.'''
+return a * h / 2
+
+def perimeter(a, b, c):
+   '''a,b,c are the lengths of the three sides of the triangle respectively.
+   Return  the perimeter of the triangle.'''
+return a + b + c
