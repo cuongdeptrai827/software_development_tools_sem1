437a5b116c3aece0dfce7c5f9b7e179141512d63
Nguyen Le Cuong <nguyenlecuongaaa@gmail.com>
2023-10-10 20:58:52 +0300

    adding comment to circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,14 @@
 import math
 
-
 def area(r):
     return math.pi * r * r
-
+'''
+    Number r the radius of the circle
+    return the value of square of the circle with formula S = pi * r^2 '''
 
 def perimeter(r):
+    ''' Number r is the radius of the circle
+        return the value of perimeter of the circle with formula S = pi * 2r
+    '''
     return 2 * math.pi * r
 
