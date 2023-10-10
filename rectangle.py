bf1c5abc5f48026cd61d599adcf2060fc58f6302
Nguyen Le Cuong <nguyenlecuongaaa@gmail.com>
2023-10-10 21:00:17 +0300

    adding comment to rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,8 @@
 def area(a, b):
+    ''' Numbers a and b are two sides of the rectangle.
+Return the result as the area of a rectangle with two sides a and b. '''
 return a * b
 def perimeter(a, b):
+'''Numbers a and b are two sides of the rectangle.
+Returning the result is the perimeter of the rectangle with 2 sides a and b. '''
 return 2(a + b)
